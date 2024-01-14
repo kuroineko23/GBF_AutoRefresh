93540604073b1effe643f4bfecb557d1c5586f1e
kuroineko23 <73019701+kuroineko23@users.noreply.github.com>
2024-01-14 11:14:45 +0700

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,5 +1,5 @@
 const regex_battle_end = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/resultmulti\/content\/index\/*");
-const regex_normal_attack = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/rest/multiraid/normal_attack_result.json")
+const regex_normal_attack = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/rest\/multiraid\/normal_attack_result.json")
 var url = ""
 chrome.storage.sync.get('redirect', (data) => {
     url = data.redirect
