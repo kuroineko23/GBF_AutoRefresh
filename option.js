07a00d5a390c1504c82413ae02051b51a1954ef2
samuel.darmadi@gmail.com <73019701+kuroineko23@users.noreply.github.com>
2024-04-08 09:56:29 +0700

    delete unused files

--- option.js
+++ option.js
@@ -1,17 +1,17 @@
 function save_options() {
   var url = document.getElementById('redirect').value;
-    chrome.storage.sync.set({
-      redirect: url
-    }, function() {
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
+  chrome.storage.sync.set({
+    redirect: url
+  }, function () {
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
       status.textContent = '';
     }, 1000);
   });
 }
 chrome.storage.sync.get('redirect', (data) => {
-  if(data.redirect != null)
+  if (data.redirect != null)
     document.getElementById('redirect').value = data.redirect
 })
 document.getElementById('save').addEventListener('click', save_options);
