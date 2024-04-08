9252578989e8548b139838e5a98aae4d5d210c7f
samuel.darmadi@gmail.com <73019701+kuroineko23@users.noreply.github.com>
2024-04-08 09:52:05 +0700

    v2

--- refresh.js
+++ refresh.js
@@ -1,37 +1,23 @@
-const regex_battle_end = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/resultmulti\/content\/index\/*");
+const regex_battle_end = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/resultmulti\/*");
 const regex_normal_attack = new RegExp("https:\/\/((game\.granbluefantasy)|(gbf\.game\.mbga))\.jp\/rest/multiraid/normal_attack_result.json")
 var url = ""
 chrome.storage.sync.get('redirect', (data) => {
     url = data.redirect
 })
 
-//redirect to specified url after reaching result screen
-chrome.devtools.network.onRequestFinished.addListener(request => {
-    request.getContent((body) => {
-        if (request.request && request.request.url) {
-            if (request.request.url.match(regex_battle_end)) {
-                var tabId = chrome.devtools.inspectedWindow.tabId;
-                chrome.tabs.update(tabId, {
-                    "url": url
-                })
-            }
-        }
-    });
-});
-
-//if normal attack kills the monster
-chrome.devtools.network.onRequestFinished.addListener(request => {
-    request.getContent((body) => {
-        if (request.request && request.request.url) {
-            if (request.request.url.match(regex_normal_attack)) {
-                const obj = JSON.parse(body)
-                obj.scenario.forEach(item => {
-                    if(item.cmd == "die" && item.to == "boss")
-                    {
-                        chrome.devtools.inspectedWindow.reload()
-                    }
-                })
-            }
-        }
-    });
-});
+chrome.webRequest.onResponseStarted.addListener(function (details) {
+    var random = Math.floor(Math.random() * 1000)
+    var tabId = details.tabId;
+    if (details.url.match(regex_normal_attack)) {
+        setTimeout(() => {
+            chrome.tabs.reload(tabId)
+        }, random)
+    }
+    if (details.url.match(regex_battle_end)) {
+        setTimeout(() => {
+            chrome.tabs.update(tabId, {
+                "url": url
+            })
+        }, random)
+    }
+}, { urls: ["<all_urls>"] });
